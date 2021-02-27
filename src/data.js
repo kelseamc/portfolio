b02d34d2466f921be09b8cfab29a30b4fe7859df
kelseamc <kelsea.mcall@gmail.com>
2021-02-27 18:50:30 -0500

    order up added

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 import piggy from './assets/piggy-screenshot.png'
+import orderup from './assets/order-up-screenshot.png'
 
 export const projectArray = [
     {
@@ -6,8 +7,14 @@ export const projectArray = [
         frontend: "https://github.com/kelseamc/piggybank-frontend",
         img: piggy,
         description: "A app to help organize and visualize your savings!",
-        languages: "React.js | Redux.js | Ruby on Rails"
+        languages: "React.js | Redux.js | Ruby on Rails | PostgreSQL"
     },
-    
+    {
+        name: "Order Up",
+        frontend: "https://github.com/kelseamc/orderupfrontend",
+        img: orderup,
+        description: "An online resteraunt ordering application",
+        languages: "React.js | Ruby on Rails | PostgreSQL"
+    },
 
 ]
